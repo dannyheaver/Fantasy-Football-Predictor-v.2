b6e6ef221741bda7eb4067a00b9d187a03bcc8f0
danny <dannyheaver99@gmail.com>
2023-05-14 17:31:31 +0100

    mapping the defenders and midfielders into attacking and defensive that better reflect their points totals

--- project/models/all_seasons.py
+++ project/models/all_seasons.py
@@ -1,6 +1,7 @@
 """
 Class to transform the unioned gameweeks data.
 """
+import numpy as np
 
 
 class AllSeasons:
@@ -9,3 +10,32 @@ class AllSeasons:
         :param dataframe: pandas.core.frame.DataFrame
         """
         self.all_seasons = dataframe
+
+    def map_defender_position(self):
+        """
+        maps the defenders into either attacking defender, i.e. full back, or defensive defender, i.e. central defender
+        important to note this isn't completely accurate in mapping the defenders position
+        :return: None
+        """
+        defenders_creativity = self.all_seasons[(self.all_seasons["position"] == "DEF")].groupby(
+            ["name"]).median(numeric_only=True).reset_index()
+        defenders_creativity["type_of_def"] = ["AttDEF" if creativity > 2 else "DefDEF" for creativity in
+                                               defenders_creativity["creativity"]]
+        gameweeks = self.all_seasons.merge(defenders_creativity[["name", "type_of_def"]], on="name", how="left")
+        self.all_seasons["position"] = np.where(gameweeks["position"] == "DEF", gameweeks["type_of_def"],
+                                                gameweeks["position"])
+
+    def map_midfielder_position(self):
+        """
+        maps the midfielders into either attacking midfielder, i.e. 10's or wingers, or defensive midfielder
+        important to note this isn't completely accurate in mapping the defenders position
+        :return: None
+        """
+        defenders_creativity = self.all_seasons[(self.all_seasons["position"] == "MID")].groupby(
+            ["name"]).median(numeric_only=True).reset_index()
+        defenders_creativity["type_of_mid"] = ["AttMID" if creativity > 2 else "DefMID" for creativity in
+                                               defenders_creativity["threat"]]
+        gameweeks = self.all_seasons.merge(defenders_creativity[["name", "type_of_mid"]], on="name",
+                                           how="left")
+        self.all_seasons["position"] = np.where(gameweeks["position"] == "MID", gameweeks["type_of_mid"],
+                                                gameweeks["position"])
--- project/transform/union.py
+++ project/transform/union.py
@@ -8,3 +8,6 @@ from project.models.all_seasons import AllSeasons
 all_gameweeks = pd.concat(
     map(lambda x: pd.read_csv(x, low_memory=False), glob.glob("../../data/clean_gameweeks/*.csv")))
 gameweeks = AllSeasons(all_gameweeks)
+
+gameweeks.map_defender_position()
+gameweeks.map_midfielder_position()
