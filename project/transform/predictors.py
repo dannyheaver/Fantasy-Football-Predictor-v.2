d6777bfe6760f476dc6526a92a59867cc3018a47
danny <dannyheaver99@gmail.com>
2023-05-21 16:43:39 +0100

    defining the predictors and training datasets

--- project/transform/predictors.py
+++ project/transform/predictors.py
@@ -2,3 +2,12 @@
 Prepare the latest gameweek data, for each player who plays for a team in the next FPL gameweek,
 for predicting.
 """
+import json
+import pandas as pd
+
+parameters = json.load(open("../../project/parameters.json"))
+CURRENT_SEASON = parameters["CURRENT_SEASON"]
+
+all_seasons = pd.read_csv("../../data/all_seasons.csv")
+
+recent_gameweek = all_seasons[(all_seasons["season"] == CURRENT_SEASON) & (all_seasons["shift_opponent"].isnull())]
--- project/transform/training.py
+++ project/transform/training.py
@@ -1,3 +1,8 @@
 """
 Prepare the all complete rows of the master data to train the model.
 """
+import pandas as pd
+
+all_seasons = pd.read_csv("../../data/all_seasons.csv", low_memory=False)
+
+training_data = all_seasons[all_seasons["shift_opponent"].notna()]
